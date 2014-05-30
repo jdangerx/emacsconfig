a2008baa3bfffe06c32ce77a152318c62f094ad4
John Xia <john.danger.xia@gmail.com>
2014-05-30 14:17:05 -0500

    things

--- elpa/deferred-20130523.1007/deferred-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; deferred-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("deferred-pkg.el" "deferred.el") (20999
-;;;;;;  58569 930645 936000))
-
-;;;***
-
-(provide 'deferred-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; deferred-autoloads.el ends here
